ecf17f4752ef5a0035076f03c30bb1ecc706495c
Mike Issaev <scr89shadow@gmail.com>
2024-02-15 17:07:27 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,1 @@
-bot_token = '6523099315:AAF-sdCi2J4p4jJtSbmcJJ7Wo0oh6la1MjQ'
-youtube_api_key = 'AIzaSyBVw0Qe483wBjY7JtS6wKPMN5TWt-CNLy0'
-csv_file_name = 'channels.csv'
-admins = [581197242]
-CHANNEL_NAME = '@newdriftvideoyt'
-CHECK_INTERVAL = 900
+
